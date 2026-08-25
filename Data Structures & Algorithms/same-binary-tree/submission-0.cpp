6a3439786ba68def109378861757052436c4f107
Vinaykumar215 <148672064+Vinaykumar215@users.noreply.github.com>
2026-08-25 11:18:25 +0530

    Add: same-binary-tree - submission-0

---
+++ Data Structures & Algorithms/same-binary-tree/submission-0.cpp
@@ -0,0 +1,30 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+
+class Solution {
+public:
+    bool isSame(TreeNode* p,TreeNode* q){
+        if(p==NULL && q==NULL) return true;
+        if((p==NULL && q != NULL) || (p != NULL && q == NULL)) return false;
+        if(p->val != q->val){
+            return false;
+        }
+        bool left=isSame(p->left,q->left);
+        bool right=isSame(p->right,q->right);
+
+        if(left && right) return true;
+        else return false;
+    }
+    bool isSameTree(TreeNode* p, TreeNode* q) {
+        return isSame(p,q);
+    }
+};
